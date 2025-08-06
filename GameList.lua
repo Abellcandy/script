c3a253d75607245da8d369591aa756c0d1267848
Abellcandy <ryansett6@gmail.com>
2025-08-06 12:33:25 -0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 return setmetatable({}, {
     __index = function()
-        return "https://raw.githubusercontent.com/Abellcandy/script/refs/heads/main/seuscript.lua"
+        return "https://raw.githubusercontent.com/Abellcandy/script-meu/refs/heads/main/futuristic_menu_script.lua"
     end
 })
